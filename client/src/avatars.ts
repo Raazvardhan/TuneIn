5fd810ab4e3ee15258495122a5fc80181f820aa2
Raj vardhan <139915269+Raazvardhan@users.noreply.github.com>
2024-07-24 01:19:04 +0530

    add

---
+++ client/src/avatars.ts
@@ -0,0 +1,7 @@
+export const PEOPLES_IMAGES = [
+  "https://img.freepik.com/free-photo/3d-cartoon-character_23-2151021912.jpg?t=st=1719288573~exp=1719292173~hmac=9704057ba5e4fff3c5d74c921950259f08238aca6e28987a9c602681cb978489&w=996",
+  "https://img.freepik.com/free-photo/3d-cartoon-character_23-2151021913.jpg?t=st=1719288621~exp=1719292221~hmac=d8019357b8420ee9229bb061c797e222c11ae49fc3222c6a702bebdfbd9dfe3d&w=360",
+  "https://img.freepik.com/free-photo/3d-cartoon-character_23-2151021904.jpg?t=st=1719288689~exp=1719292289~hmac=1d354b3f5e155b121cb66538522cb689eb7ae6b3f94fa84a9bda44f1002a0b8d&w=360",
+  "https://img.freepik.com/free-photo/3d-cartoon-character_23-2151021933.jpg?t=st=1719288813~exp=1719292413~hmac=079c828ff70f6af065bde11d73db71116464ea4318c34bc66017e30e758c3627&w=360"
+  ,"https://img.freepik.com/free-photo/3d-cartoon-character_23-2151021955.jpg?t=st=1719288432~exp=1719292032~hmac=e0c6048c591e146b34d02bfd8a41311fdff3a013d3978b7afa083f6cd2d35137&w=996",
+];
